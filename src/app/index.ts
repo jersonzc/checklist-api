445d5c14ab6b4cfdba6a36a274befedd26b165a1
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-03-14 10:39:33 -0500

    feat: use error handler
    return JSON

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,31 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
 
 export const app = express();
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World!');
+  res.json({ message: 'Welcome to the API' });
 });
 
 // No route found
 app.use((req, res, next) => {
-  res.status(404);
-  res.json({
+  next({
+    status: 404,
     message: 'Error. Route not found!',
   });
 });
+
+// Error handler
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: NextFunction,
+  ) => {
+    const { status = 500, message } = err;
+    res.status(status);
+    res.json({
+      error: { status, message },
+    });
+  },
+);
