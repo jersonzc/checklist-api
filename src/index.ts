32c6fd65026b1bcf68038e392ca4a569b3b21531
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-03-14 10:17:57 -0500

    refactor: style

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ const hostname = '127.0.0.1';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello World!");
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
