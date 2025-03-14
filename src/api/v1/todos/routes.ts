74da02cd0ba1c0ae8184f206b30844bebcfbc209
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-03-14 11:21:59 -0500

    feat: define resources

--- src/api/v1/todos/routes.ts
+++ src/api/v1/todos/routes.ts
@@ -2,8 +2,21 @@ import { Router } from 'express';
 
 export const router = Router();
 
-router.route('/').get((req, res, next) => {
-  res.json({
-    message: 'GET all todos',
-  });
-});
+/*
+ * /api/todos/ POST - Create
+ * /api/todos/ GET - Read all
+ * /api/todos/:id GET - Read one
+ * /api/todos/:id PUT - Update
+ * /api/todos/:id DELETE - Delete
+ */
+
+router
+  .route('/')
+  .post((req, res, next) => {})
+  .get((req, res, next) => {});
+
+router
+  .route('/:id')
+  .get((req, res, next) => {})
+  .put((req, res, next) => {})
+  .delete((req, res, next) => {});
