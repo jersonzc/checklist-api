4145e7cee9a09b5b3abf1d695c829acf361e7583
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-03-14 10:33:37 -0500

    feat: handle route not found

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,14 @@ import express from 'express';
 
 export const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Hello World!');
+});
+
+// No route found
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Error. Route not found!',
+  });
 });
