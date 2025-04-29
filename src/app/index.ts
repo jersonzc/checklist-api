1563bc1ddcfbcf014631ebeea077f208003171cc
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-04-28 22:01:43 -0500

    feat: return request id on response header

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 import { router as api } from '../api/v1/index.js';
 import { ErrorResponse } from '../types.js';
@@ -7,6 +8,15 @@ export const app = express();
 
 // Parse JSON
 app.use(express.json());
+
+// Request ID
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const id = uuidv4();
+    res.setHeader('X-Request-Id', id);
+    next();
+  },
+);
 
 // Setup API routes
 app.use('/api', api);
