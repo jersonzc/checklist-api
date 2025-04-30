d6b0ab65c38a12a0481da0b02bbb5bdd475b5046
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-04-29 19:09:08 -0500

    test: util functions

--- src/app/util.test.ts
+++ src/app/util.test.ts
@@ -1,12 +1,38 @@
-import { parsePaginationParams } from './utils.js';
+import { parsePaginationParams, parseSortParams } from './utils.js';
+import { expect } from 'vitest';
 
 describe('Utils', () => {
-  test('parsePaginationParams', () => {
-    const result = parsePaginationParams({ limit: '10' });
+  describe('parsePaginationParams', () => {
+    test('with only one param', () => {
+      const result = parsePaginationParams({ limit: '10' });
 
-    expect(result).toEqual({
-      limit: 10,
-      offset: 0,
+      expect(result).toEqual({
+        limit: 10,
+        offset: 0,
+      });
+    });
+    test('without params', () => {
+      const result = parsePaginationParams({});
+
+      expect(result).toEqual({
+        limit: 10,
+        offset: 0,
+      });
+    });
+  });
+
+  describe('parseSortParams', () => {
+    test('with valid params', () => {
+      const result = parseSortParams({
+        fields: ['id', 'title'],
+        direction: 'asc',
+        orderBy: 'title',
+      });
+
+      expect(result).toEqual({
+        orderBy: 'title',
+        direction: 'asc',
+      });
     });
   });
 });
