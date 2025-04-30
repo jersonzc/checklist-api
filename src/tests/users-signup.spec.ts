ead1788d42c72290eba3694756a87afde82e150f
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-04-29 20:10:46 -0500

    test: signup and signin user

--- src/tests/users-signup.spec.ts
+++ src/tests/users-signup.spec.ts
@@ -8,14 +8,20 @@ describe('Users signup', () => {
     await resetDb();
   });
 
-  test('signed up successfully', async () => {
+  test('signed in successfully', async () => {
     const agent = request(app);
     const body = {
       name: 'Juanita',
       email: 'juanita@gmail.com',
       password: 'Aa123456',
     };
-    const user = await agent.post('/api/users/signup').send(body);
-    expect(user.status).toBe(200);
+    const signup = await agent.post('/api/users/signup').send(body);
+    expect(signup.status).toBe(200);
+
+    const signin = await agent.post('/api/users/signin').send({
+      email: body.email,
+      password: body.password,
+    });
+    expect(signin.status).toBe(200);
   });
 });
