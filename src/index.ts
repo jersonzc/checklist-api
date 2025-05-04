9a9f1bdec5a3a11447823b5ef1804ef8e29c00dc
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-05-04 18:59:16 -0500

    feat: listen on every address

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,11 @@ import { connect } from './app/database.js';
 // Connect to the database
 connect();
 
-const hostname = '127.0.0.1';
 const { port } = configuration.server;
 
 // Create server
 const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at ${port} port`);
 });
